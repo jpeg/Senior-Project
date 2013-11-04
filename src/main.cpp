7ee0beef2c5ea65b9a4ccc814857aaea6b828826
Jason Gassel <pegelite1@gmail.com>
2013-11-04 00:10:34 -0600

    Added thread for camera detection

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "ConfigManager.h"
 #include "Camera.h"
 #include "DetectObject.h"
 #include "EmailWrapper.h"
 
+void* cameraThreadMain(void* arg);
+
 int main(int argc, char** argv)
 {
     ConfigManager::init();
     
-    if(EmailWrapper::sendEmail(ConfigManager::emailRecipient, "Test Email", "This is a test message.", ConfigManager::gmailUsername, ConfigManager::gmailPassword, "README.md"))
-    {
-        printf("Sent email.\n");
-    }
-    else
-    {
-        printf("Failed to send email.\n");
-    }
+    // Start camera thread
+    pthread_t* cameraThread = new pthread_t;
+    pthread_attr_t* cameraThreadAttr = new pthread_attr_t;
+    pthread_attr_init(cameraThreadAttr);
+    pthread_create(cameraThread, cameraThreadAttr, cameraThreadMain, NULL);
     
     ConfigManager::save();
     
     return 0;
+}
+
+void* cameraThreadMain(void* arg)
+{
+    Camera* raspiCam = new Camera();
+    raspiCam->init(DetectObject::IMAGE_WIDTH, DetectObject::IMAGE_HEIGHT);
+    
+    DetectObject* detectObject = new DetectObject();
+    detectObject->init();
+    
+    // Train object detection
+    detectObject->resetTrainingGray();
+    const int trainingFrames = 50;
+    for(int i=0; i<trainingFrames; i++)
+    {
+        raspiCam->captureFrame();
+        detectObject->trainGray(raspiCam->getLastFrame());
+    }
+    
+    return NULL;
 }
 
